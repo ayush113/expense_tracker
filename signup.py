d58883301a79d65bb9472b5b10a177b766fd125f
Ayush Kumar <ayush.kumar1999@hotmail.com>
2017-05-07 15:32:40 +0530

    Add files via upload

---
+++ signup.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect('user.sqlite3')
+cur=conn.cursor()
+
+try:
+    cur.execute('CREATE TABLE users (user_id TEXT, password TEXT,security_q TEXT,security_a TEXT)')
+except:
+    print "\n"
+
+
+def start():
+    user = dict()
+    user["id"] = raw_input("Enter a username")
+    user["password"] = raw_input("Enter your password")
+    user["q"]=raw_input("SECURITY QUESTION")
+    user["a"]=raw_input("YOUR ANSWER")
+
+    cur.execute('INSERT INTO users (user_id,password,security_q,security_a) VALUES (?,?,?,?)',(user["id"],user["password"],user["q"],user["a"]))
+
+
+def cut():
+    cur.execute('SELECT * FROM users')
+
+    for row in cur:
+        print row,"\n"
+    
